1970c684423e52471f029723f311fcfaacbc4544
Peter Shen <petershen0307@hotmail.com>
2014-10-25 22:12:24 +0800

    [What] Add parsing price from web.

---
+++ parsing_price.py
@@ -0,0 +1,71 @@
+import re
+import urllib.request
+from numbers import Number
+from pathlib import Path
+
+# combine with stock number and to witch year
+twsePriceUrl = 'http://www.twse.com.tw/ch/trading/exchange/FMNPTK/FMNPTK2.php?STK_NO={stockNumber}&myear={untilYear}&mmon=07&type=csv'
+def getStockPriceFromWeb(stockId, years):
+    urlWithStockAndYear = twsePriceUrl.format(stockNumber = stockId, untilYear = years)
+    try :
+        webMessage = urllib.request.urlopen(urlWithStockAndYear)
+    except :
+        print('URL error')
+        print(urlWithStockAndYear)
+        return False
+    #rawdata type is bytes, it need to decode
+    rawdata = webMessage.read()
+    return rawdata.decode('big5')
+
+resultDir = Path('./result_CSV')
+def getStockYearAndPrice(stockId, years, duringYears = 5):
+    parsingYears = '年度'
+    parsingAvgPrice = '收盤平均價'
+    csvData = getStockPriceFromWeb(stockId, years)
+    if False == csvData :
+        return False
+    lineList = csvData.replace(' ', '').split('\n')
+    #remove garbage messages
+    removeIndex = [0, len(lineList)-1, len(lineList)-2, len(lineList)-3, len(lineList)-4]
+    lineList = [i for j, i in enumerate(lineList) if j not in removeIndex]
+
+    #parse title line
+    yearIndex = 0
+    avgIndex = 0
+    titleLine = lineList.pop(0).split(',')
+    try:
+        yearIndex = titleLine.index(parsingYears)
+        avgIndex = titleLine.index(parsingAvgPrice)
+    except ValueError:
+        print('pass')
+        return False
+
+    #get the stock price
+    resultList = [[parsingYears, parsingAvgPrice]]
+    avgPrices = []
+    for each in lineList :
+        tmp = re.sub(r'"[\d,]+"', 'xx', each).split(',')
+        resultList.append([int(tmp[yearIndex]), float(tmp[avgIndex])])
+        avgPrices.append(float(tmp[avgIndex]))
+    print(resultList)
+
+    totalYears = len(avgPrices)
+    if totalYears < duringYears or 0 == duringYears:
+        totalAvgPrice = float(sum(avgPrices) / totalYears)
+    else :
+        #[first : second]  list
+        # last duringYears
+        totalAvgPrice = float(sum(avgPrices[(totalYears - duringYears) : totalYears]) / duringYears)
+    resultList.append(['', totalAvgPrice])
+
+    if not resultDir.exists() :
+        resultDir.mkdir()
+    with open(str(resultDir) + '/avgPrice_{Id}.csv'.format(Id = stockId), 'w', encoding = 'big5') as writeData :
+        for eleList in resultList :
+            for eleStr in eleList :
+                #detect the type which is Number
+                if isinstance(eleStr, Number) :
+                    writeData.write(str(eleStr) + ',')
+                else :
+                    writeData.write(eleStr + ',')
+            writeData.write('\n')
